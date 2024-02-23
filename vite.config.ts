2552eb1f50a8a75430d616d0c7532e739cd50353
Joao Souza <joao.souza@avenuecode.com>
2024-02-23 04:51:55 -0300

    config vite app

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/locadora-test/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    hmr: {
-      host: '0.0.0.0',
-      port: 5173
+// https://vitejs.dev/config/ƒ
+export default defineConfig(({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log(env)
+  return {
+    base: '/locadora-test/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      hmr: env.VITE_BUILD_MODE === 'build' ? { host: '0.0.0.0', port: 5173 } : {}
     }
   }
 })
